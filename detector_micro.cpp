1f12096ef9767217a4d521734f7c289ba99547df
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2026-04-26 14:18:05 +0200

    v1.5

--- detector_micro.cpp
+++ detector_micro.cpp
@@ -24,10 +24,10 @@
  */
 
 /* ── TFLite Micro fejlécek ──────────────────────────────────── */
-#include "tflm_includes/tensorflow/lite/micro/micro_interpreter.h"
-#include "tflm_includes/tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tflm_includes/tensorflow/lite/micro/system_setup.h"
-#include "tflm_includes/tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 
 /* ── Generált modell tömb (xxd -i models/model.tflite > models/model.h) */
 #include "models/model.h"
